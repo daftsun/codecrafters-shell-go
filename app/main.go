834936db99bfd9b69e1429d95ae2e6a0cd133a60
Aditya Gupta <aditya27gupta@gmail.com>
2026-05-12 23:25:22 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,41 +4,48 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"slices"
 	"strings"
 )
 
 var builtins = []string{"exit", "echo", "type"}
 
-func handle_command(cmd string) {
-	if cmd == "exit" {
-		os.Exit(0)
-	} else if after, found := strings.CutPrefix(cmd, "echo"); found {
-		fmt.Print(strings.TrimSpace(after))
-	} else if after, found := strings.CutPrefix(cmd, "type"); found {
-		command := strings.TrimSpace(after)
-		if slices.Contains(builtins, command) {
-			fmt.Printf("%s is a shell builtin", command)
-		} else {
-			fmt.Printf("%s: not found", command)
-		}
-
-	} else {
-		fmt.Printf("%s: command not found", cmd)
-	}
-	fmt.Print("\n")
-}
-
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("$ ")
-		command, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading input: %s", err)
-			os.Exit(1)
+		line, _ := reader.ReadString('\n')
+
+		fields := strings.Fields(line)
+		if len(fields) == 0 {
+			continue
 		}
-		command = strings.TrimSpace(command)
-		handle_command(command)
+
+		command := fields[0]
+		switch command {
+		case "exit":
+			return
+		case "echo":
+			fmt.Println(strings.Join(fields[1:], " "))
+		case "type":
+			type_command(fields[1:])
+		default:
+			fmt.Printf("%s: command not found\n", command)
+		}
+	}
+}
+
+func type_command(args []string) {
+	if len(args) > 0 {
+		for _, arg := range args {
+			if slices.Contains(builtins, arg) {
+				fmt.Printf("%s is a shell builtin\n", arg)
+			} else if path, _ := exec.LookPath(arg); path != "" {
+				fmt.Printf("%s is %s\n", arg, path)
+			} else {
+				fmt.Printf("%s: not found\n", arg)
+			}
+		}
 	}
 }
