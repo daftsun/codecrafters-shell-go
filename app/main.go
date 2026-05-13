10ba01d1afbb6bd947096f6e192f475c7898f4e7
Aditya Gupta <aditya27gupta@gmail.com>
2026-05-13 19:33:08 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,6 +12,19 @@ import (
 var builtins = []string{"exit", "echo", "type", "pwd"}
 
 func main() {
+
+	cmdFuncMap := map[string]func(shellArgs []string){
+		"exit": func(shellArgs []string) { os.Exit(0) },
+		"echo": func(shellArgs []string) { fmt.Println(strings.Join(shellArgs, " ")) },
+		"type": handleType,
+		"pwd": func(shellArgs []string) {
+			pwd, err := os.Getwd()
+			if err == nil {
+				fmt.Println(pwd)
+			}
+		},
+	}
+
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("$ ")
@@ -27,18 +40,8 @@ func main() {
 		}
 
 		command := fields[0]
-		if command == "exit" {
-			return
-		} else if command == "echo" {
-			fmt.Println(strings.Join(fields[1:], " "))
-		} else if command == "type" {
-			handleType(fields[1])
-		} else if command == "pwd" {
-			cwd, err := os.Getwd()
-			if err != nil {
-				fmt.Print(err)
-			}
-			fmt.Printf("%s\n", cwd)
+		if cmdFunc, ok := cmdFuncMap[command]; ok {
+			cmdFunc(fields[1:])
 		} else if _, err := exec.LookPath(command); err == nil {
 			cmd := exec.Command(command, fields[1:]...)
 			cmd.Stdout = os.Stdout
@@ -50,12 +53,14 @@ func main() {
 	}
 }
 
-func handleType(command string) {
-	if slices.Contains(builtins, command) {
-		fmt.Printf("%s is a shell builtin\n", command)
-	} else if path, err := exec.LookPath(command); err == nil {
-		fmt.Printf("%s is %s\n", command, path)
-	} else {
-		fmt.Printf("%s: not found\n", command)
+func handleType(shellArgs []string) {
+	for _, command := range shellArgs {
+		if slices.Contains(builtins, command) {
+			fmt.Printf("%s is a shell builtin\n", command)
+		} else if path, err := exec.LookPath(command); err == nil {
+			fmt.Printf("%s is %s\n", command, path)
+		} else {
+			fmt.Printf("%s: not found\n", command)
+		}
 	}
 }
